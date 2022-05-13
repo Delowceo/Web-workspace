2f4837b20f323ea609b64660b9efd26a0e3a0deb
Delowceo <hermanmbwilo99@gmail.com>
2022-05-13 15:38:49 +0300

    basic form

---
+++ forms/form.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+  <title>BASIC FORM</title>
+  <link rel="stylesheet" href="../styles/bootstrap.min.css">
+</head>
+<body>
+  <div class="container">
+    <form>
+      <p style="color:blue">Registration form </p>
+      <div class="form-group">
+        <label class="label"for="name "> Name</label>
+        <input type="text" name="name" class="form-control">
+      </div>
+      <div class="form-group">
+        <label class="label"for="email "> Email</label>
+        <input type="email" name="Email" class="form-control">
+      </div>
+      <div class="form-group">
+        <label class="label"for="password "> Password</label>
+        <input type="password" name="Password" class="form-control">
+      </div>
+      <div class="form-group mt-3">
+        <input type="submit" name="submit" value="submit" class="btn btn-success">
+      </div>
+    </form>
+</body>
+</html>
